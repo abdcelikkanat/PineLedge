precision of the remiander fixed..

src/base.py
@@ -292,11 +292,14 @@ class BaseModel(torch.nn.Module):
         term0 = 0.5 * torch.sqrt(torch.as_tensor(utils.PI, device=self._device)).to(self._device) * inv_norm_delta_v
         term1 = torch.exp(beta_ij.unsqueeze(0) + (r ** 2) - (norm_delta_xt ** 2))
 
-        upper_bounds = interval[1:].clone() % self._bin_width
-        upper_bounds[interval[1:] == self._bin_width] = self._bin_width
-        #upper_bounds[interval_idx[1:] > 0] = upper_bounds[interval_idx[1:] > 0] - interval_idx[1:][interval_idx[1:] > 0] * self._bin_width
-        lower_bounds = interval[:-1].clone() % self._bin_width
-        #lower_bounds[interval_idx[:-1] > 0] = lower_bounds[interval_idx[:-1] > 0] - interval_idx[:-1][interval_idx[:-1] > 0] * self._bin_width
+        # TORCH HAS PRECISION PROBLEM IN REMAINDER OPERATIONS
+        upper_bounds = interval[1:].clone() #% self._bin_width
+        upper_bounds = ((self._bins_num * upper_bounds).type(dtype=torch.int) % 1) / float(self._bins_num) #upper_bounds = ((self._bins_num * upper_bounds) % (self._bins_num*self._bin_width)) / float(self._bins_num)
+        upper_bounds[(self._bins_num*interval[1:]).type(dtype=torch.int) % 1 <= utils.EPS] = self._bin_width #upper_bounds[(self._bins_num*interval[1:]) % (self._bins_num*self._bin_width) <= utils.EPS] = self._bin_width = self._bin_width
+        upper_bounds[interval[1:] == 0] = 0
+        lower_bounds = interval[:-1].clone()
+        #lower_bounds[(self._bins_num*interval[:-1]) % (self._bins_num*self._bin_width) <= utils.EPS] = 0
+        lower_bounds[(self._bins_num * interval[:-1]).type(dtype=torch.int) % 1 <= utils.EPS] = 0
         term2_u = torch.erf(upper_bounds.unsqueeze(
             1) * norm_delta_v + r)
         term2_l = torch.erf(lower_bounds.unsqueeze(

tests/integral.py
@@ -28,7 +28,7 @@ class AlgebraicEquivalence(unittest.TestCase):
     #
     #     dim = 2
     #     nodes_num = 2
-    #     bins_num = 2
+    #     bins_num = 100
     #     # x0 = 0.5*torch.eye(nodes_num) #utils.mean_normalization(torch.rand(size=(nodes_num, dim)))
     #     # v = torch.vstack((-1.0*torch.eye(nodes_num).unsqueeze(0), 1.0*torch.eye(nodes_num).unsqueeze(0))) #utils.mean_normalization(torch.rand(size=(bins_num, nodes_num, dim)))
     #     x0 = torch.randn(size=(nodes_num, dim))
@@ -36,7 +36,6 @@ class AlgebraicEquivalence(unittest.TestCase):
     #
     #     x0 = utils.mean_normalization(x0)
     #     v = utils.mean_normalization(v)
-    #     print("v: ", v)
     #
     #     beta = torch.zeros(size=(nodes_num, ))
     #
@@ -74,11 +73,11 @@ class AlgebraicEquivalence(unittest.TestCase):
 
         dim = 2
         nodes_num = 2
-        bins_num = 2
-        x0 = 0.5*torch.eye(nodes_num) #utils.mean_normalization(torch.rand(size=(nodes_num, dim)))
-        v = torch.vstack((-1.0*torch.eye(nodes_num).unsqueeze(0), 1.0*torch.eye(nodes_num).unsqueeze(0))) #utils.mean_normalization(torch.rand(size=(bins_num, nodes_num, dim)))
-        # x0 = torch.randn(size=(nodes_num, dim))
-        # v = torch.randn(size=(bins_num, nodes_num, dim))
+        bins_num = 100
+        # x0 = 0.5*torch.eye(nodes_num) #utils.mean_normalization(torch.rand(size=(nodes_num, dim)))
+        # v = torch.vstack((-1.0*torch.eye(nodes_num).unsqueeze(0), 1.0*torch.eye(nodes_num).unsqueeze(0))) #utils.mean_normalization(torch.rand(size=(bins_num, nodes_num, dim)))
+        x0 = torch.randn(size=(nodes_num, dim))
+        v = torch.randn(size=(bins_num, nodes_num, dim))
 
         x0 = utils.mean_normalization(x0)
         v = utils.mean_normalization(v)
@@ -90,8 +89,8 @@ class AlgebraicEquivalence(unittest.TestCase):
                        prior_B_sigma = 2 * torch.rand(size=(1,)) - 1,
                        prior_C_Q = torch.rand(size=(4, nodes_num)),
         )
-        tl = 0.0 #+ utils.EPS
-        tr = 0.35 #- + utils.EPS
+        tl = 0.66 #+ utils.EPS
+        tr = 0.98 #- + utils.EPS
         # exact = bm.get_intensity_integral_for(i=0, j=1, interval=torch.as_tensor([tl, tr]))
         # exact = bm.get_intensity_integral(
         #     nodes=torch.as_tensor([0, 1]),