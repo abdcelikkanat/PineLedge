79a1de102d47dbc85630c3ac70c996333b679bb8
abdecelikkanat <abdecelikkanat@gmail.com>
2022-05-24 10:53:53 +0200

    server...

--- experiments/examination.py
+++ experiments/examination.py
@@ -21,32 +21,39 @@ import matplotlib.pyplot as plt
 # Set some paremeters
 dim = 2
 K = 10
-bins_num = 3
-prior_lambda = 1e5 #1e9 #1e5
-batch_size = 20  #1
-learning_rate = 0.01
-epochs_num = 1000 #1000  # 300
+bins_num = 50
+prior_lambda = 1e5
+#batch_size = 20  #1
+learning_rate = 0.001
+epochs_num = 2000  # 500
 steps_per_epoch = 1
-seed = utils.str2int("why") # testing_dyad_sequential_dec all_events # survival_true_with_event # nhpp testing_seq2
+seed = utils.str2int("full")
 verbose = True
 shuffle = True
 suffix = "" #f"_percent={0.1}" #f"_percent={0.01}" #f"_percent={0.2}" #"_nhpp" #"_survival"
 ###
-dataset_name = f"two_clusters_fp_sizes=10_10_beta=2" #f"four_nodes_fp" #f"two_clusters_fp_sizes=10_10_beta=1" #f"three_clusters_sizes=15_20_10" # sbm_survival three_clusters_fp_sizes=15_20_10_beta=0
-model_name = f"{dataset_name}_D={dim}_B={bins_num}_K={K}_pl={prior_lambda}_lr={learning_rate}_e={epochs_num}_spe={steps_per_epoch}_s={seed}{suffix}"
+#dataset_name= f"two_clusters_fp_sizes=10_10_beta=2" #f"four_nodes_fp" #f"two_clusters_fp_sizes=10_10_beta=1" #f"three_clusters_sizes=15_20_10" # sbm_survival three_clusters_fp_sizes=15_20_10_beta=0
+dataset_name = f"ia-contact" 
+#dataset_name = f"ia-contacts_hypertext2009"
+test_intervals_num = 50
+
 
 # Define dataset and model path
-dataset_folder = os.path.join(
-    utils.BASE_FOLDER, "datasets", "synthetic", dataset_name
-)
-
-model_path = os.path.join(os.path.join(utils.BASE_FOLDER, "experiments", "models", model_name), f"{model_name}.model")
+#dataset_folder = os.path.join(utils.BASE_FOLDER, "datasets", "synthetic", dataset_name)
+dataset_folder = os.path.join(utils.BASE_FOLDER, "datasets", "real", dataset_name)
+samples_file_path=os.path.join(f"/work3/abce/workspace/pivem/samplesv{test_intervals_num}/", dataset_name)
+#samples_file_path=os.path.join(utils.BASE_FOLDER, f"baselines/samplesv{test_intervals_num}", dataset_name)
 
 ########################################################################################################################
 
 # Load the dataset
 all_events = Events(seed=seed)
 all_events.read(dataset_folder)
+
+batch_size=all_events.number_of_nodes()
+model_name = f"{dataset_name}_D={dim}_B={bins_num}_K={K}_pl={prior_lambda}_lr={learning_rate}_e={epochs_num}_spe={steps_per_epoch}_s={seed}{suffix}"
+model_path = os.path.join(os.path.join(utils.BASE_FOLDER, "experiments", "models", model_name), f"{model_name}.model")
+
 
 # Normalize the events
 all_events.normalize(init_time=0, last_time=1.0)
@@ -64,13 +71,22 @@ lm = LearningModel(data=data, nodes_num=nodes_num, bins_num=bins_num, dim=dim, l
 
 # Load the model
 assert os.path.exists(model_path), "The model file does not exist!"
-lm.load_state_dict(torch.load(model_path))
+lm.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
 
 ########################################################################################################################
+
+with open(os.path.join(samples_file_path, "samples.pkl"), 'rb') as f:
+    samples_data = pkl.load(f)
+    #valid_labels = samples_data["valid_labels"]
+    #valid_samples = samples_data["valid_samples"]
+    labels = samples_data["test_labels"]
+    samples = samples_data["test_samples"]
+
+'''
 # Construct samples
 
 threshold = 1
-test_intervals_num = 10
+test_intervals_num = 8
 test_intervals = torch.linspace(0, 1.0, test_intervals_num+1)
 
 samples, labels = [[] for _ in range(test_intervals_num)], [[] for _ in range(test_intervals_num)]
@@ -86,7 +102,7 @@ for i, j in utils.pair_iter(n=nodes_num):
             labels[b].append(1)
         else:
             labels[b].append(0)
-
+'''
 ########################################################################################################################
 # Predicted scores
 pred_scores = [[] for _ in range(test_intervals_num)]
@@ -98,11 +114,12 @@ for b in range(test_intervals_num):
 
 
 for b in range(test_intervals_num):
-
-    roc_auc = roc_auc_score(y_true=labels[b], y_score=pred_scores[b])
-    print(f"Roc AUC, Bin Id {b}: {roc_auc}")
-    pr_auc = average_precision_score(y_true=labels[b], y_score=pred_scores[b])
-    print(f"PR AUC, Bin Id {b}: {pr_auc}")
+    if len(labels[b]) != sum(labels[b]) and sum(labels[b]) != 0:
+        roc_auc = roc_auc_score(y_true=labels[b], y_score=pred_scores[b])
+        print(f"Roc AUC, Bin Id {b}: {roc_auc}")
+        pr_auc = average_precision_score(y_true=labels[b], y_score=pred_scores[b])
+        print(f"PR AUC, Bin Id {b}: {pr_auc}")
+        print("")
 
 roc_auc_complete = roc_auc_score(
     y_true=[l for bin_labels in labels for l in bin_labels],
@@ -114,3 +131,17 @@ pr_auc_complete = average_precision_score(
     y_score=[s for bin_scores in pred_scores for s in bin_scores]
 )
 print(f"PR AUC in total: {pr_auc_complete}")
+
+'''
+nodes_num = all_events.number_of_nodes()
+intensities = lm.get_intensity_integral(nodes=torch.arange(nodes_num))
+
+M = torch.zeros(size=(nodes_num, nodes_num))
+for i, j in utils.pair_iter(n=nodes_num):
+    M[i, j] = intensities[utils.pairIdx2flatIdx(i=i, j=j, n=nodes_num)]
+
+plt.figure()
+plt.matshow(M)
+plt.colorbar()
+plt.show()
+'''
--- experiments/learn_representations.py
+++ experiments/learn_representations.py
@@ -5,28 +5,47 @@ from src.learning import LearningModel
 from torch.utils.data import DataLoader
 from src.events import Events
 
+# global control for device
+CUDA = True
+# availability for different devices
+avail_device="cuda:0" if torch.cuda.is_available() else "cpu"
+
+# choosing device and setting default tensor, meaning that each new tensor has a default device pointing to
+if (CUDA) and (avail_device == "cuda:0"):
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+else:
+    torch.set_default_tensor_type('torch.FloatTensor')
+
 
 # Set some paremeters
 dim = 2
 K = 10
-bins_num = 3
+bins_num = 100
 prior_lambda = 1e5
-batch_size = 20  #1
-learning_rate = 0.01
+#batch_size = 20  #1
+learning_rate = 0.01 #0.001
 epochs_num = 1000  # 500
 steps_per_epoch = 1
-seed = utils.str2int("why")
+seed = utils.str2int("full")
 verbose = True
 shuffle = True
 
 ###
-dataset_name = f"two_clusters_fp_sizes=10_10_beta=2" #"four_nodes_fp" #f"two_clusters_fp_sizes=10_10_beta=1" #f"fb_forum" #f"three_clusters_fp_sizes=15_20_10"
-model_name = f"{dataset_name}_D={dim}_B={bins_num}_K={K}_pl={prior_lambda}_lr={learning_rate}_e={epochs_num}_spe={steps_per_epoch}_s={seed}"
+#dataset_name = f"two_clusters_fp_sizes=10_10_beta=2" #"four_nodes_fp" #f"two_clusters_fp_sizes=10_10_beta=1" #f"fb_forum" #f"three_clusters_fp_sizes=15_20_10"
+dataset_name = "ia-contact"
+#dataset_name = "ia-contacts_hypertext2009"
 
 # Define dataset and model path
-dataset_path = os.path.join(
-    utils.BASE_FOLDER, "datasets", "synthetic", dataset_name
-)
+#dataset_path = os.path.join(utils.BASE_FOLDER, "datasets", "synthetic", dataset_name)
+dataset_path = os.path.join(utils.BASE_FOLDER, "datasets", "real", dataset_name)
+
+# Load the dataset
+all_events = Events(seed=seed)
+all_events.read(dataset_path)
+batch_size=all_events.number_of_nodes()
+
+model_name = f"{dataset_name}_D={dim}_B={bins_num}_K={K}_pl={prior_lambda}_lr={learning_rate}_e={epochs_num}_spe={steps_per_epoch}_s={seed}"
+
 model_folder = os.path.join(
     utils.BASE_FOLDER, "experiments", "models", model_name
 )
@@ -34,9 +53,6 @@ model_path = os.path.join(
     model_folder, f"{model_name}.model"
 )
 
-# Load the dataset
-all_events = Events(seed=seed)
-all_events.read(dataset_path)
 
 # Normalize the events
 all_events.normalize(init_time=0, last_time=1.0)
